945beedef49351cedbbab6cd824e02586ef5e25b
Ben Sorscher <bsorsch@node18-ccncluster.stanford.edu>
2019-09-24 13:53:56 -0700

    updated trainer for tf2

--- trainer.py
+++ trainer.py
@@ -2,119 +2,75 @@
 import tensorflow as tf
 import numpy as np
 
+from visualize import save_ratemaps
+from tqdm.autonotebook import tqdm
+
 
 class Trainer(object):
-    def __init__(self, model, flags):
+    def __init__(self, options, model, data_manager):
+        self.options = options
         self.model = model
-        self._prepare_optimizer(flags)
-        self._prepare_summary()
-        self.flags=flags
+        self.data_manager = data_manager
+        # self.loss_fun = tf.keras.metrics.categorical_crossentropy
+        self.loss_fun = tf.nn.softmax_cross_entropy_with_logits
+        self.acc_fun = tf.keras.metrics.categorical_accuracy
+        lr = self.options['learning_rate']
+        self.optimizer = tf.keras.optimizers.Adam(learning_rate=lr)
 
-    def _prepare_optimizer(self, flags):
-        with tf.variable_scope("opt"):
-            output_vars = tf.trainable_variables("model/outputs")
+        self.loss = []
+        self.acc = []
 
-            # Apply L2 regularization to output linear layers
-            l2_out = tf.add_n([
-                tf.nn.l2_loss(v) for v in output_vars
-                if 'bias' not in v.name
-            ]) * flags.l2_reg 
-
-
-            # # VAR OUT
-            # W = tf.trainable_variables('model/outputs/dense/kernel')[0]
-            # Wout = tf.reduce_sum(W**2, axis=-1)
-            # var = tf.reduce_mean((Wout-tf.reduce_mean(Wout))**2) * flags.l2_reg
-
-            optimizer = tf.train.AdamOptimizer(
-                learning_rate=flags.learning_rate,
-            )
-            
-            # Nonnegativity constraint on g (breaking the g -> -g symmetry)
-            nonneg_g = -tf.reduce_sum(tf.minimum(self.model.g, 0)) * flags.nonneg_obj
-
-            # # l2 constraint on g
-            l2_g = tf.nn.l2_loss(self.model.g) * flags.l2_reg
-        
-            # l2 constraint on input weights
-            # l2_win = tf.nn.l2_loss(tf.trainable_variables('model/dense/kernel')) * flags.l2_reg
-            # l2_win += tf.nn.l2_loss(tf.trainable_variables('model/dense_1/kernel'))
-
-
-            # Whitening penalty
-            # G = self.model.g
-            # C = tf.matmul(tf.transpose(G), G)
-            # Ctri = C - tf.diag_part(C) * tf.eye(flags.rnn_size)
-            # white_penalty = tf.reduce_sum(Ctri**2) * 1e-5
-
-            # G = self.model.g
-            # C = tf.matmul(tf.transpose(G), G)
-            # white_penalty = tf.reduce_sum((C-tf.eye(flags.rnn_size))**2) * 1e-3
-
-
-            # Frobenius norm on recurrent weights
-            # Jall = tf.trainable_variables('model/rnn/basic_rnn_cell/kernel')[0]
-            # J = tf.gather(Jall, np.arange(flags.rnn_size)+2)
-            # frob_loss = tf.trace(tf.matmul(J, tf.transpose(J))) * flags.frobenius
-
-
-            # Antisymmetric penalty
-            # asymm = tf.reduce_sum((J - tf.transpose(J))**2) * flags.l2_reg
-
-
-            total_loss = self.model.place_loss
-#                 self.model.hd_loss \
-#                 + nonneg_g  \
-#                 + l2_g
-                # + white_penalty
-
-
-            # Compute gradients
-            gvs = optimizer.compute_gradients(total_loss)
-
-#             clipped_gvs = []
-#             for grad, var in gvs:
-#                 if "model/outputs" in var.name:
-#                     gv = (tf.clip_by_value(grad,
-#                                            -flags.gradient_clipping,
-#                                            flags.gradient_clipping), var)
-#                 # # Train recurrent weights more slowly
-#                 # elif "basic_rnn_cell/kernel" in var.name:
-#                 #     gv = (1e-2*grad, var)
-#                 else:
-#                     gv = (grad, var)
-#                 clipped_gvs.append(gv)
-                
-            self.train_op = optimizer.apply_gradients(gvs)
-    
-            # # Only train velocity weights
-            # J = tf.trainable_variables('model/rnn/basic_rnn_cell/kernel')
-            # mask = np.zeros([flags.rnn_size+3, flags.rnn_size])
-            # mask[:3] = 1
-            # [(grad, var)] = optimizer.compute_gradients(total_loss, var_list=[J])
-            # self.train_op = optimizer.apply_gradients([(grad*mask, var)])
-
-    def _prepare_summary(self):
-        with tf.name_scope("logs"):
-            tf.summary.scalar("place_loss", self.model.place_loss)
-            # tf.summary.scalar("hd_loss", self.model.hd_loss)
-            tf.summary.scalar("place_accuracy", self.model.place_accuracy)
-            # tf.summary.scalar("hd_accuracy", self.model.hd_accuracy)
-        self.summary_op = tf.summary.merge_all()
-
-    def train(self, sess, summary_writer, data_manager, step, flags, box_width=None, box_height=None):
-        if not box_width:
-            box_width = flags.box_width
-        if not box_height:
-            box_height = flags.box_height
-            
-        # Train a batch
-        if flags.train_or_test=='test':
-            feed_dict = data_manager.feed_dict(box_width, box_height)
-            _, summary_str = sess.run([self.train_op, self.summary_op], feed_dict=feed_dict)
+        # Set up checkpoints
+        self.ckpt = tf.train.Checkpoint(step=tf.Variable(0), optimizer=self.optimizer, net=model)
+        ckpt_dir = options['save_dir'] + '/' + options['run_ID'] + '/ckpts'
+        self.ckpt_manager = tf.train.CheckpointManager(self.ckpt, ckpt_dir, max_to_keep=100)
+        self.ckpt.restore(self.ckpt_manager.latest_checkpoint)
+        if self.ckpt_manager.latest_checkpoint:
+            print("Restored from {}".format(self.ckpt_manager.latest_checkpoint))
         else:
-            _, summary_str = sess.run([self.train_op, self.summary_op])
+            print("Initializing from scratch.")
 
-        if step % 10 == 0:
-            summary_writer.add_summary(summary_str, step)
 
+    def compute_loss(self, labels, preds):
+        '''Compute cross-entropy loss'''
+        loss = tf.reduce_mean(self.loss_fun(labels, preds))
+        acc = tf.reduce_mean(self.acc_fun(labels, preds))
+        return loss, acc
+
+
+    def train_step(self, inputs, pc_outputs):
+        with tf.GradientTape() as tape:
+            preds = self.model(inputs)
+            loss, acc = self.compute_loss(pc_outputs, preds)
+
+        grads = tape.gradient(loss, self.model.trainable_variables)
+        self.optimizer.apply_gradients(zip(grads, self.model.trainable_variables))
+        
+        return loss, acc
+
+
+    def train(self, n_epochs=10, n_steps=100, save=True):
+        # Construct generator
+        gen = self.data_manager.get_generator()
+
+        for epoch in tqdm(range(n_epochs)):
+            t = tqdm(range(n_steps), leave=True)
+            for _ in t:
+                inputs, pc_outputs = next(gen)
+                loss, acc = self.train_step(inputs, pc_outputs)
+                self.loss.append(loss)
+                self.acc.append(acc)
+                
+                t.set_description('Acc = ' + str(np.round(100*acc, 1)) + '%')
+
+                self.ckpt.step.assign_add(1)
+
+            if save:
+                # Save checkpoint
+                self.ckpt_manager.save()
+                tot_step = self.ckpt.step.numpy()
+
+                # Save a picture of rate maps
+                save_ratemaps(self.model, self.data_manager, self.options, step=tot_step)
+
+            
